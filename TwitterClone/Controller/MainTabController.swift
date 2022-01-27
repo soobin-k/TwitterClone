da3f27f43e0a81a85c0d9ac3cf6de0619d07b29d
soobin-k <tnqls27qks@naver.com>
2022-01-28 02:19:24 +0900

    Initial Commit

---
+++ TwitterClone/Controller/MainTabController.swift
@@ -0,0 +1,17 @@
+//
+//  MainTabController.swift
+//  TwitterClone
+//
+//  Created by 김수빈 on 2022/01/28.
+//
+
+import UIKit
+
+class MainTabController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemPink
+    }
+}
